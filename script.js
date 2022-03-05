bec7e50889f1456c08d921bebe2e2987dd3ec36d
Zaigham Salehi <zsalehi313@gmail.com>
2022-03-05 17:20:07 -0500

    Randomly generate rock, paper or scissors for computer

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log(`hello, world.`);
+// declare values for rock, paper, scissors
+const rock = 1;
+const paper = 2;
+const scissors = 3;
+
+
+// function computerPlay; randomly return either rock, paper or scissors
+function computerPlay () {
+    let randomNum = Math.floor(Math.random()*3) + 1;
+    let selection;
+    // define if statement that will print rock, paper, scissors when randonNum = rock, paper or scissors
+    if (randomNum === rock) {
+        selection = "Rock";
+    } else if (randomNum === paper) {
+        selection = "Paper";
+    } else {
+        selection = "Scissors";
+    }
+
+    return selection;
+}
+
